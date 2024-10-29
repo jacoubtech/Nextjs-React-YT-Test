ce4871c8760bbbb64b3845c3553543006369ed6a
jacoubtech <fortas.yacoub85@gmail.com>
2024-10-29 06:40:32 +0100

    Routing add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+
+
+function About(){
+  return(
+  <div>
+       <h2> About Page</h2>
+       <Link href="/">Go to Home </Link>
+    </div>
+    )
+}
+export default About ;
--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,19 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Link from "next/link";
 
 
 function Home() {
 let hidetext = true; 
 
   return (
-      <main>
-        <Header name="Islem Fortas" />
-        {hidetext ? <div><Footer footname="Da best"/></div> : null}
-        {/* <Footer footname="hi" />   */}
-      </main>
+      <><main style={{ color: "teal", backgroundColor: "gold" }}>
+      <Header name="Islem Fortas" />
+      {hidetext ? <div><Footer footname="Da best" /></div> : null}
+      {/* <Footer footname="hi" />   */}
+    </main>
+    <h2><Link href="/about">Go to About</Link></h2></>
   );
 }
 export default Home;
