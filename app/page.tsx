432c27075dfe9f2dfd6a08dcef9af8d00d17f318
Islem Fortas <fortas.yacoub85@gmail.com>
2024-10-29 14:05:50 +0100

    use state Hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,39 @@
+'use client';
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Link from "next/link";
+import { useState } from "react";
 
 
 function Home() {
 let hidetext = true; 
+const [counter, setCounter] = useState(0);
 
+const handleClick = () => {
+  setCounter (counter + 1) ;
+}
+const moinshundleclick = () => {
+ 
+    setCounter(counter - 1); 
+ 
+}
+const reset = () => {
+  setCounter(0);
+} 
   return (
       <><main style={{ color: "teal", backgroundColor: "gold" }}>
       <Header name="Islem Fortas" />
       {hidetext ? <div><Footer footname="Da best" /></div> : null}
-      {/* <Footer footname="hi" />   */}
+      
     </main>
-    <h2><Link href="/about">Go to About</Link></h2></>
+    <h2><Link href="/about">Go to About</Link></h2>
+    <div>Counter : {counter} </div>
+    <button onClick={handleClick}>add</button> 
+    <button onClick={reset}>reset </button>
+    {counter == 0 ? setCounter(0) :<button onClick={moinshundleclick}>moins</button>}
+    </>
   );
 }
 export default Home;
